4241fe362ec1060a74c55bde77488a712cf0d116
Ccc <a695667978@163.com>
2018-04-23 23:02:30 +0800

    jq第三天

--- js/ajax.js
+++ js/ajax.js
@@ -1,83 +1,24 @@
-$.getJSON('http://localhost:3000/service', function (data) {
-  // for (var i = 0; i < data.length; i++) {
-  //   console.log(data[i].imgUrl)
-  //   // $('<img src="' + data[i].imgUrl + '">').appendTo('#pic')
-  //   // console.log($(this)[0].imgUrl[1])
-  //   $('#pic1').append('<div class="content_pic">' +
-  //       '<a href="">' +
-  //         '<img src="'+data[i].imgUrl+'" alt="">'+
-  //       '</a>'+
-  //     '</div>')
-  //
-  // }
-  // var i = 0
-  // $(data.serve1).each(function () {
-  //   console.log($(this))
-  //   if(i === 5){
-  //     return
-  //   }else {
-  //     i += 1
-  //   }
-  //   console.log(i)
-  //   console.log($('#pic'+i))
-  //   console.log($(this)[0])
-  //   $('#pic'+i).append('<div class="content_pic">' +
-  //     '<a href="">' +
-  //       '<img src="'+ $(this)[0].imgUrl+'" alt="">'+
-  //      '</a>'+
-  //   '</div>')
-  // })
-  // $(data).each(function (index, item) {
-  //   console.log($(this))
-  //   console.log(index, item)
-  //   // if(index === data.length - 1){
-  //   //   return
-  //   // }
-  //   console.log(index)
-  // })
-  var a = -1
-  console.log(data)
-  $.each(data, function (i, item) {
-    // console.log(data.serve1)
-    $(data).attr('partner', '')
-    console.log(i, item)
-    if(a === 8){
-      return
-    }else {
-      console.log(a)
-      $('#c'+a).append('<div class="pic_wrap" id="pic'+a+'"></div>')
-      a++
+
+$.getJSON('http://localhost:3000/servicepro', function (data) {
+  var data1 =data.servicepro
+  var dataDom=''
+  for (var i = 0; i < data1.length; i++) {
+    console.log(data1[i])
+    dataDom += '<li>\n' +
+                    '<a>\n' +
+                      '<img src="'+data1[i].imgUrl+'">\n' +
+                      '<p class="top">\n'+
+                        '<img src="'+data1[i].iconUrl+'" alt="">\n' +
+                        '<span>'+data1[i].name+'</span>\n'+
+                      '</p>\n'+
+                      '<p class="bottom">\n'+
+                      '已接单'+data1[i].order+'单\n'+
+                      '<i>好评'+data1[i].estimate+'%</i>\n'+
+                      '</p>\n'+
+                    '</a>\n'+
+                 '</li>\n'
     }
-    // $(item).each(function (index, list) {
-    //        console.log(a);
-    //        console.log(list.imgUrl)
-    //        console.log($('#pic'+ a))
-    //        console.log($('#w1'))
-    //        $('#pic'+ a + '').addClass('aaa')
-    //     })
-    // setTimeout(function(){
-    //   $(item).each(function (index, list) {
-    //     console.log(a);
-    //     console.log(list.imgUrl)
-    //     console.log($('#pic'+ a))
-    //     console.log($('#w1'))
-    //     $('#pic'+ a).addClass('aaa')
-    //   })
-    // },2000);
-    // $('#c'+a).delegate(item+ a, 'each', function () {
-    //   $('#pic'+ a).addClass('aaa')
-    // })
-    $(item).each(function (index, list) {
-      console.log(a);
-      console.log(list.imgUrl)
-      console.log($('#pic'+ a))
-      var pic = document.getElementById('pic'+a)
-      $('#pic'+ a).addClass('aaa')
-    })
-  })
+  $('#serve_pic ul').html(dataDom)
 })
-.error(function () {
-  $('#pic1').append('<div class="content_pic">'+
-    '<p>数据未找到</p>'+
-  '</div>')
-})
+
+ 
